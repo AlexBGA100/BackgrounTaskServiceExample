61a684c600d6829ef9bff688ed2192192899d26b
Alexandr Юрченко <Alexbga100@gmail.com>
2025-02-06 01:59:41 +0200

    Добавьте файлы проекта.

---
+++ BackgrounTaskServiceExample/Controllers/WeatherForecastController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackgrounTaskServiceExample.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+       
+    }
+}
---
+++ Infrastructure/Background/BackgroundTaskQueue.cs
@@ -0,0 +1,6 @@
+﻿namespace Infrastructure.Background
+{
+    public class BackgroundTaskQueue
+    {
+    }
+}
